683bc68f29e085fdf6a470a765f5a61d6d17c5a8
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-12-09 22:18:06 +0100

    quizz idea added

---
+++ AppEduNewApproach/Models/QuizA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEduNewApproach.Models
+{
+    class QuizA
+    {
+        public int QuizAId { get; set; }
+        public string QuizAAdress { get; set; }
+        public string QuizzAName { get; set; }
+        public string QuizzAButton { get; set; }
+        // mechanim sprawdzania jeśli button się zgadza wyświetla planszę wynik i dodaje punkty
+    }
+}
